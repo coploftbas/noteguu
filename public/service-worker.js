730f3301220774c48344af8c5a6ef3f5eaddce91
Kevin Tantisevi <pongpanot.t@nexts-corp.com>
2016-11-20 00:27:17 +0700

    try service worker

---
+++ public/service-worker.js
@@ -0,0 +1,56 @@
+// Set a name for the current cache
+var CACHE_NAME = 'NOTE-GUU-V1';
+
+// Default files to always cache
+var urlsToCache = [
+    '/bower_components/polymer/polymer.html',
+    '/bower_components/app-route/app-location.html',
+    '/bower_components/app-route/app-route.html',
+    '/bower_components/iron-pages/iron-pages.html',
+
+    '/bower_components/polymerfire/firebase-app.html',
+    '/bower_components/polymerfire/firebase-document.html',
+    '/bower_components/polymerfire/firebase-query.html',
+    '/bower_components/polymerfire/firebase-messaging.html',
+
+    '/bower_components/paper-input/paper-input',
+    '/bower_components/paper-input/paper-textarea.html',
+    '/bower_components/paper-button/paper-button.html'
+];
+
+
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('[ServiceWorker] Caching cacheFiles');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        
+        caches.match(event.request)
+        .then(function (response) {
+            // Cache hit - return response
+            if (response) {
+                console.log('[ServiceWorker] Found in Cache : ',response.url)
+                return response;
+            }
+
+
+            // IMPORTANT: Clone the request. A request is a stream and
+            // can only be consumed once. Since we are consuming this
+            // once by cache and once by the browser for fetch, we need
+            // to clone the response.
+            var fetchRequest = event.request.clone();
+            console.log('[ServiceWorker] Cache Not Found :',fetchRequest.url);
+            return fetch(fetchRequest)
+        })
+
+    );
+});
